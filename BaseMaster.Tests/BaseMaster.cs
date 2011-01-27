eca9acd4278df40698c248a4313285cbb751eda1
robertgreiner <robert@robertgreiner.com>
2011-01-26 18:20:28 -0600

    Added empty solution and test project

---
+++ BaseMaster.Tests/BaseMaster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseMaster.Tests {
+    [TestClass]
+    public class BaseMaster {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
